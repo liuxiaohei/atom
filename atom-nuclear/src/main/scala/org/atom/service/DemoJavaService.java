93475afca2b7f2b56291b84e4ee65c85d4e69d9b
ld <liudi@welian.com>
2018-11-26 21:39:58 +0800

    third

---
+++ atom-nuclear/src/main/scala/org/atom/service/DemoJavaService.java
@@ -0,0 +1,27 @@
+package org.atom.service;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.routing.SmallestMailboxPool;
+import org.atom.config.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DemoJavaService {
+
+    @Autowired
+    private ActorSystem actorSystem;
+    @Autowired
+    private SpringExtension springExtension;
+
+    public void detect(String detectStatus, int detectCount) {
+
+        ActorRef routerActorRef = actorSystem.actorOf(springExtension.props("detectActor2")
+                .withDispatcher("detect-dispatcher")
+                .withRouter(new SmallestMailboxPool(50000)), "detectRouterActor");
+
+        System.out.println("into detect function,resultActorRef=" + routerActorRef.path());
+
+
+        //routerActorRef.tell(new MapMessage(instance, connectMap), ActorRef.noSender());
+    }
+}
