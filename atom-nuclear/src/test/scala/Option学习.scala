c578c1c4c4d27d7e042124b48a7a62b3b1fa6ab2
ld <liudi@welian.com>
2018-12-27 20:05:34 +0800

    third

---
+++ atom-nuclear/src/test/scala/Option学习.scala
@@ -0,0 +1,39 @@
+import org.junit.Test
+
+class Option学习 {
+
+  def toInt(s: String): Option[Int] = {
+    try {
+      Some(Integer.parseInt(s.trim))
+    } catch {
+      case e: Exception => None
+    }
+  }
+
+  @Test
+  def demo1: Unit ={
+    println(toInt("aaa").getOrElse("-1"))
+    println(toInt("1").getOrElse("-1"))
+  }
+
+  @Test
+  def demo2: Unit ={
+    toInt("1").foreach {
+      i => println(i)
+    }
+  }
+
+  @Test
+  def demo3: Unit ={
+    toInt("1") match {
+      case Some(i) => println(i)
+      case None => println("That didn't work.")
+    }
+
+    toInt("aaa") match {
+      case Some(i) => println(i)
+      case None => println("That didn't work.")
+    }
+  }
+}
+
