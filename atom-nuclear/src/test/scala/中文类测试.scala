18283c323ff400af186c92647f5c9372dd62b690
ld <liudi@welian.com>
2018-12-27 19:03:25 +0800

    third

--- atom-nuclear/src/test/scala/中文类测试.scala
+++ atom-nuclear/src/test/scala/中文类测试.scala
@@ -1,10 +1,13 @@
+import beans.DemoBean
 import org.junit.Test
 
 class 中文类测试 {
 
   @Test
   def 样例 {
-    println("测试一下")
+    val demobean = new DemoBean
+    demobean.setName("小黑喵")
+    println(demobean.getName)
   }
 
 }
